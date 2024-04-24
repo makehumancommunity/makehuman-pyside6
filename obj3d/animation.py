1ba96953e7e6270e911eb16a7bddb436638a4640
black-punkduck <ralf-rednitzhembach@t-online.de>
2024-04-24 20:40:38 +0200

    added an internal way to support channels for poses

--- obj3d/animation.py
+++ obj3d/animation.py
@@ -1,19 +1,36 @@
+import numpy as np
 
 class BVHJoint():
     def __init__(self, name):
         self.name = name
         self.end = name is None
-        self.channels = []
+        self.parent = None
+        self.nChannels = 0
+        self.children = []
+        self.animdata = None
+
+        # which channels are used, will contain index [-1, -1, -1, 0, 1, 2] = Xrotation, Yrotation, Zrotation
+        #
+        self.channelorder = [-1,-1,-1,-1,-1,-1]
+
+        # offset
+        #
+        self.offset = np.zeros(3,dtype=np.float32)  # fixed relative offset
 
     def __repr__(self):
-        return (self.name if self.name is not None else "end-site")
+        output = self.name if self.name is not None else "end-site"
+
+        return (output + " " + str(self.channelorder) + " " + str(self.animdata))
 
 class BVH():
     def __init__(self, glob, name):
         self.glob = glob
+        self.env = glob.env
         self.name = name
+        self.channelname = {"Xposition":0, "Yposition":1, "Zposition":2, "Xrotation":3, "Yrotation":4, "Zrotation":5}
         self.bvhJointOrder = []
         self.joints = {}
+        self.frameCount = 1 # dummy frame
 
     def keyParam(self, key, fp):
         param = fp.readline().split()
@@ -26,7 +43,24 @@ class BVH():
         if name is not None:
             self.joints[name] = joint
         self.bvhJointOrder.append(joint)
+        joint.parent = parent
+        if parent is not None:
+            parent.children.append(joint)
         return (joint)
+
+    def getChannelOrder(self, joint, fp):
+        (param, msg ) = self.keyParam('CHANNELS', fp)
+        if param is None:
+            return (False, msg)
+        nChannels = int(param[0])
+        if nChannels != len(param)-1:
+            return (False, "Channels indicated and number of channels differ")
+        for cnt, channel in enumerate(param[1:]):
+            if channel in self.channelname:
+                joint.channelorder[self.channelname[channel]] = cnt
+        joint.nChannels = nChannels
+        return (True, "okay")
+
 
     def readJointHierarchy(self, joint, fp):
         (param, msg ) = self.keyParam('{', fp)
@@ -39,15 +73,11 @@ class BVH():
 
         # Calculate position from offset
         #
-        offset = [float(x) for x in param]
+        joint.offset = [param[0], param[1], param[2]]
 
-        (param, msg ) = self.keyParam('CHANNELS', fp)
-        if param is None:
+        (err, msg ) = self.getChannelOrder(joint, fp)
+        if err is False:
             return (False, msg)
-        nChannels = int(param[0])
-        joint.channels = param[1:]
-        if nChannels != len(joint.channels):
-            return (False, "Channels indicated and number of channels differ")
 
         # Read child joints
         while True:
@@ -55,17 +85,17 @@ class BVH():
             words = line.split()
 
             if words[0] == 'JOINT':
-                child = self.addJoint(words[1], joint.name)
+                child = self.addJoint(words[1], joint)
                 self.readJointHierarchy(child, fp)
             elif words[0] == 'End': # Site
-                child = self.addJoint(None, joint.name)
+                child = self.addJoint(None, joint)
                 (param, msg ) = self.keyParam('{', fp)
                 if param is None:
                     return (False, msg)
                 (param, msg ) = self.keyParam('OFFSET', fp)
                 if param is None:
                     return (False, msg)
-                offset = [float(x) for x in param]
+                child.offset = [param[0], param[1], param[2]]
                 (param, msg ) = self.keyParam('}', fp)
                 if param is None:
                     return (False, msg)
@@ -76,8 +106,30 @@ class BVH():
 
         return (True, "okay")
 
+    def initFrames(self):
+        for joint in self.bvhJointOrder:
+            joint.animdata = np.zeros(shape=(self.frameCount, 6), dtype=np.float32)
+
+    def fillFrames(self, frame, data):
+        #
+        i = 0
+        for joint in self.bvhJointOrder:
+            if joint.nChannels > 0:
+                for j, m in enumerate(joint.channelorder):
+                    if m>=0:
+                        r = data[i+m]
+                        if -0.0001 < r < 0.0001:
+                            r = 0.0
+                        joint.animdata[frame, j ] = r
+                i += joint.nChannels
+
+
+    def debugJoints(self):
+        for joint in self.bvhJointOrder:
+            print (joint)
+
     def load(self, filename):
-        print ("Load " + filename)
+        self.env.logLine(1, "Load pose " + filename)
 
         with open(filename, "r", encoding='utf-8') as fp:
             # starts with HIERARCHIE 
@@ -108,11 +160,13 @@ class BVH():
                 return (False, msg)
             self.frameTime = float(param[1])
 
+            self.initFrames()
+
             for i in range(self.frameCount):
                 words = fp.readline().split()
                 data = [float(word) for word in words]
-                print (len(data))
+                self.fillFrames(i, data)
 
-        print (self.bvhJointOrder)
+        self.debugJoints()
         return (True, "Okay")
 
